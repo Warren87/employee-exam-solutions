6c5700b6826e73edb6ce50dd03c59a85b9674fc8
tomaszprzybylski <tomasz.przybylski@intive.com>
2022-09-26 09:09:12 +0200

    Missing to string for building

--- src/main/kotlin/employees/Building.kt
+++ src/main/kotlin/employees/Building.kt
@@ -3,6 +3,13 @@ package employees
 sealed class Building(
         val roomsCount: Int,
         val hasKitchen: Boolean,
-        val bathroomsCount: Int)
+       val bathroomsCount: Int) {
+
+    override fun toString() = """
+ *** Building status ***
+ Building has $roomsCount room(s) and ${if(hasKitchen) "" else "no"} and $bathroomsCount bathroom(s).
+ *** End building status ***
+    """.trimIndent()
+}
 
 
